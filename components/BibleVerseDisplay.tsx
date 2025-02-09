d1d2c03fd13ae2b869a27cf1ea8c719d9d6c9b6b
Nicolae Iosif <7108834+iosifnicolae2@users.noreply.github.com>
2025-02-09 21:48:07 +0200

    refactor: remove disconnected message from Bible verse display

--- components/BibleVerseDisplay.tsx
+++ components/BibleVerseDisplay.tsx
@@ -101,15 +101,6 @@ export function BibleVerseDisplay({ verses, currentBook }: BibleVerseDisplayProp
               </View>
 
 
-              <View style={[styles.disconnectedMessage, { position: 'absolute', bottom: 20, left: 0, right: 0 }]}>
-                <Text style={[styles.disconnectedText, {
-                  fontSize: fontSize * 0.9,
-                  color: '#999999',
-                  textAlign: 'center'
-                }]}>
-                  Dispozitivul a pierdut conexiunea
-                </Text>
-              </View>
             </>
         )}
       </View>
